ebc3cbafcf3ca108752e423328f5c86c0c99ff37
eric.hu <eric.hu@theronts.biz>
2020-10-22 15:28:45 +0800

    add perfect function

--- demo1/index.js
+++ demo1/index.js
@@ -1,7 +1,35 @@
-const array = [1, 1, 2, 3, 4, 4, 5, 3, 9, 3, {}, {}];
+/*
+ * 数组去重，建议使用hasOwnProperty来判断
+ *
+ */
+const array = [
+  1,
+  1,
+  "true",
+  "true",
+  true,
+  true,
+  15,
+  15,
+  false,
+  false,
+  undefined,
+  undefined,
+  null,
+  null,
+  NaN,
+  NaN,
+  "NaN",
+  0,
+  0,
+  "a",
+  "a",
+  {},
+  {},
+];
 
-// 1.一次循环，再通过indexOf找出新数组中不存在的就插入新数组
-function getUniqueArray(array) {
+// 1.一次循环，再通过indexOf找出新数组中不存在的就插入新数组 (这种方法还无法去掉“{}”空对象)
+function getUniqueArrayByIndexOf(array) {
   let newArray = [];
   for (let index = 0; index < array.length; index++) {
     const element = array[index];
@@ -12,8 +40,8 @@ function getUniqueArray(array) {
   return newArray;
 }
 
-// 2.两层循环，再通过splice删除相同元素
-function getUniqueArray1(array) {
+// 2.两层循环，再通过splice删除相同元素 (这种方法还无法去掉“{}”空对象)
+function getUniqueArrayBySplice(array) {
   for (let index = 0; index < array.length; index++) {
     const element = array[index];
     for (let flag = index + 1; flag < array.length; flag++) {
@@ -22,16 +50,40 @@ function getUniqueArray1(array) {
         array.splice(flag, 1);
         flag--;
       }
-      console.log(array);
+      //   console.log(array);
     }
   }
   return array;
 }
 
-// 3.通过es6的set,这种方法还无法去掉“{}”空对象
-function getUniqueArray2(array) {
+// 3.通过es6的set (这种方法还无法去掉“{}”空对象)
+function getUniqueArrayBySet(array) {
   //   return Array.from(new Set(array));
   return [...new Set(array)];
 }
 
-console.log(getUniqueArray2(array));
+// 4.通过hasOwnProperty来判断属性 (可以去掉)
+function getUniqueArrayByHasOwnProperty(array) {
+  let obj = {},
+    newArray = [];
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    if (!obj.hasOwnProperty(typeof element + element)) {
+      //这里这个element+element很关键
+      obj[typeof element + element] = null;
+      newArray.push(element);
+    }
+  }
+  return newArray;
+}
+
+// 5.通过filter+hasOwnProperty
+function getUniqueArrayByFilterAndHasOwnProperty(array) {
+  let obj = {};
+  return array.filter((element, index, array) => {
+    return obj.hasOwnProperty(typeof element + element)
+      ? false
+      : (obj[typeof element + element] = true);
+  });
+}
+console.log(getUniqueArrayByFilterAndHasOwnProperty(array));
